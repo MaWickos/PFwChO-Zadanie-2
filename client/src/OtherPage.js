8440ff62dbef009f290bc7b5a9493f1b250bc5da
MaWickos <maciejwicha8@gmail.com>
2023-01-24 11:25:41 +0100

    Add source files

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      <h1>mwmultifib</h1>
+      <h1>Maciej Wicha</h1>
+      <h1>IMST I2S 1.5</h1>
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
